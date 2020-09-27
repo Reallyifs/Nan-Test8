be71dba12d78f57e66f2d85169e4e8b017294134
Awsiten <52651839+Reallyifs@users.noreply.github.com>
2020-09-27 18:48:16 +0800

    初次上传
    2020.9.27

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Test8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mod Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 42);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(256, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(116, 71);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(256, 23);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 10F);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(40, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 14);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Homepage:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 14);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Author:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 14);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "buildIgnore:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(256, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "展开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(116, 129);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(256, 23);
+            this.textBox4.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 14);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Display Name:";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("宋体", 10F);
+            this.checkBox1.Location = new System.Drawing.Point(12, 158);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(124, 18);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Open          ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 183);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(96, 18);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = ".gitignore";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "*.csproj",
+            "*.dll",
+            "*.exe",
+            "*.sln",
+            "*.user",
+            ".git/*",
+            ".gitignore",
+            ".vs/*",
+            "bin/*",
+            "obj/*",
+            "README.md"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(360, 238);
+            this.checkedListBox1.Sorted = true;
+            this.checkedListBox1.TabIndex = 12;
+            this.checkedListBox1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(12, 255);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 24);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "返回";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 207);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(131, 18);
+            this.checkBox3.TabIndex = 14;
+            this.checkBox3.Text = "New description";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(63, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 24);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "确定";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 231);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(89, 18);
+            this.checkBox4.TabIndex = 16;
+            this.checkBox4.Text = "New build";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 338);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(343, 14);
+            this.linkLabel1.TabIndex = 17;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "低技术力生草屑作，看个乐呵就好，源码地址点此查看";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Font = new System.Drawing.Font("宋体", 10F);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 400);
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto-git";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Test8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in Controls)
+                control.Visible = false;
+            checkedListBox1.Visible = true;
+            button2.Visible = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in Controls)
+                control.Visible = true;
+            checkedListBox1.Visible = false;
+            button2.Visible = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Location = new Point((Width / 2) - (button2.Width / 2), (int)(((Height / 2) - (button2.Height / 2)) * 1.5));
+            button3.Location = new Point((Width / 2) - (button3.Width / 2), (int)(((Height / 2) - (button3.Height / 2)) * 1.5));
+            for (int i = 0; i < checkedListBox1.Items.Count; i++)
+                checkedListBox1.SetItemChecked(i, true);
+            checkedListBox1.Items.AddRange(new object[]
+            {
+                @"Ignores/*",
+                @"Instantiation/*",
+                @"Libraries/*",
+                @"Uselesses/*"
+            });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog()
+            {
+                Description = "选择存放的文件夹，默认运行目录",
+                SelectedPath = Directory.GetCurrentDirectory(),
+                ShowNewFolderButton = true
+            };
+            dialog.ShowDialog();
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(textBox1.Text[i]))
+                {
+                    string s1 = textBox1.Text.Substring(0, i);
+                    string s2 = textBox1.Text.Substring(i + 1);
+                    textBox1.Text = s1 + s2;
+                    i = 0;
+                }
+            }
+            File.WriteAllLines($@"{dialog.SelectedPath}\README.md", new string[]
+            {
+                $"# {textBox1.Text}",
+                $"",
+                $"{textBox4.Text}",
+                $"",
+                $"Author: {textBox3.Text}",
+                $"",
+                $"Homepage: {textBox2.Text}"
+            });
+            if (checkBox2.Checked)
+            {
+                List<string> ignoreText = new List<string>();
+                for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+                {
+                    string s3 = checkedListBox1.CheckedItems[i].ToString();
+                    if (s3 != "README.md" && s3 != @".git\*" && s3 != ".gitignore")
+                        ignoreText.Add(checkedListBox1.CheckedItems[i].ToString());
+                }
+                File.WriteAllLines($@"{dialog.SelectedPath}\.gitignore", ignoreText.ToArray());
+            }
+            if (checkBox3.Checked)
+            {
+                File.WriteAllLines($@"{dialog.SelectedPath}\description.txt", new string[]
+                {
+                    $"{textBox4.Text}",
+                    $"Author: {textBox3.Text}"
+                });
+            }
+            if (checkBox4.Checked)
+            {
+                string ignoreText = "";
+                for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+                {
+                    if (i != 0)
+                        ignoreText += ", ";
+                    ignoreText += checkedListBox1.CheckedItems[i];
+                }
+                File.WriteAllLines($@"{dialog.SelectedPath}\build.txt", new string[]
+                {
+                    $@"author = {textBox3.Text}",
+                    $@"version = 0.1.0.0",
+                    $@"hideCode = true",
+                    $@"homepage = {textBox2.Text}",
+                    $@"includePDB = false",
+                    $@"buildIgnore = {ignoreText.Replace('/', '\\')}",
+                    $@"displayName = {textBox4.Text}",
+                    $@"hideResources = true",
+                    $@"includeSource = false"
+                });
+            }
+            if (checkBox1.Checked)
+                Process.Start(dialog.SelectedPath);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/Reallyifs/Nan-Test8");
+        }
+    }
+}
